4d5153931d9ca24d95aae24036b81e9a1689d320
Aybike Ece Eren <aybikeceren@gmail.com>
2022-03-28 00:09:16 +0200

    Setting up the database connection & creating Plate model

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:parola321@localhost/parking_permit_db'
+migrate = Migrate(app, db)
+
+class Plate(db.Model):
+    plate = db.Column(db.String, primary_key=True)
+    owner = db.Column(db.String, nullable = True)
+    start_time = db.Column(db.DateTime, nullable = True)
+    end_time = db.Column(db.DateTime, nullable = True)
+
+    def __repr__(self):
+        return f"Plate: {self.plate}"
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run()
