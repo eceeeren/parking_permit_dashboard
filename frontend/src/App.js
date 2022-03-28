66c287b4134f0e45b510a983287390dfcbffdb63
Aybike Ece Eren <aybikeceren@gmail.com>
2022-03-28 16:11:26 +0200

    Frontend part added by create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import {format} from "date-fns";
+import './App.css';
+
+const baseUrl = "http://localhost:5000"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
