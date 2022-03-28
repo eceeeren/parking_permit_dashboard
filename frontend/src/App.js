3c7bc976a7e1bfc211fed597121e204abf052d41
Aybike Ece Eren <aybikeceren@gmail.com>
2022-03-28 16:30:42 +0200

    A simple event is constructed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 import {format} from "date-fns";
 import './App.css';
@@ -5,10 +6,35 @@ import './App.css';
 const baseUrl = "http://localhost:5000"
 
 function App() {
+
+  const [plate, setPlate] = useState("")
+  const [owner, setOwner] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+
+  const handleChange = e => {
+    setPlate(e.target.value);
+  }
+
+  const handleSubmit =  e => {
+    e.preventDefault();
+    console.log(plate)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-    
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="plate">Plate:</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="plate"
+            id="plate"
+            value={plate}
+          /><br/>
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
